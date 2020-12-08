ef05b27eeec0dbfa9549de1b97ab3af1d521f39f
William Schuy <william.schuy@pcc.edu>
2020-12-08 13:47:13 -0800

    Created initial functions

---
+++ genshinArtifactCalc.py
@@ -0,0 +1,17 @@
+__Author__ = "William Schuy"
+__Date__ = "Dec 7th 2020"
+
+#Program Purpose: To calculate the estimated Resin, Mora, and Artifact EXP requirements of finding an artifact you want in Genshin Impact.
+# 
+#
+
+def initializeArtifact(int rarity, string set, string mainStat, string offStatA, string offStatB, string offStatC, string offStatD)
+
+def greeting()
+def rollArtifactSlot()
+def rollArtifactMainStat(string artifactSlot)
+def rollInitialOffStat(string mainStat)
+def rollOffStat(string offStat)
+def desiredArtifactInput()
+def main()
+
