74d0b141c12c5aba691f37a9db99b4c9ef2d86ff
Maggie Dreyer <darthmagisus@gmail.com>
2020-12-13 15:21:38 -0800

    Configure linter
    This commit adds a config file for flake8, in order to configure it to
    skip linting the `context.py` in the test directory, which does some
    importing funny business that the linter disagrees with. (If we decide
    later that it is right and we should change how we're handling that, we
    can remove that exclusion from the config.)
    
    It also makes the GHA step fail instead of warn, to ensure we fix
    our errors for now.

--- tests/artifact_calc_test.py
+++ tests/artifact_calc_test.py
@@ -2,8 +2,8 @@ import re
 
 from .context import artifact_calc
 
+
 def test_artifact_init(capsys):
     artifact_calc.initializeArtifact(4, "", "", "", "", "", "", "")
     output = capsys.readouterr().out
-    assert re.search("^4", output) != None
-
+    assert re.search("^4", output) is not None
