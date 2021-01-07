bc03eed3fd6f4027fb24b57b6049b91be05b387b
William Schuy <william.schuy@pcc.edu>
2021-01-07 07:17:22 -0800

    Refactor Input Validation

--- resin_calculator/artifact_calc.py
+++ resin_calculator/artifact_calc.py
@@ -15,6 +15,9 @@ valid_main_stats_by_slot = {
     "goblet": ["hp%", "def%", "atk%", "elemental mastery", "elemental damage", "physical damage"]
 }
 
+#Not sure if valid substats should be placed in the sub stat validation function, or if I should leave as is.
+valid_sub_stats = ["atk", "hp", "def", "atk%", "hp%", "def%", "elemental mastery", "crit rate", "crit damage", "recharge"]
+
 class Artifact:
     def __init__(self, rarity, artifact_set, artifact_slot, main_stat, off_stats):
         self.rarity = rarity
@@ -26,7 +29,7 @@ class Artifact:
 
 def user_input(message):
     user_response = input(message)
-    user_response = user_response.lower()
+    user_response = user_response.lower() #converts all inputs to lowercase to error-proof user inputs
     return user_response
 
 def greeting():
@@ -34,15 +37,16 @@ def greeting():
 
 def select_artifact_slot():
     valid_slot = False
-    slots = ["circlet", "timepiece", "goblet", "feather", "flower"]
+    valid_artifact_slots = ["circlet", "timepiece", "goblet", "feather", "flower"]
     while valid_slot == False:
         artifact_slot = user_input("What slot is your artifact?\n")
-        if artifact_slot in slots:
-            valid_slot = True
+        if validate_artifact_slots(artifact_slot, valid_artifact_slots):
+            return artifact_slot
         else:
             print("Invalid artifact slot. Options are \"circlet\", \"timepiece\", \"goblet\", \"feather\", or \"flower\"\n")
-    
-    return artifact_slot
+
+def validate_artifact_slots(artifact_slot, valid_slots):
+    return artifact_slot in valid_slots
 
 def validate_rarity(rarity):
     return rarity == "3" or rarity == "4" or rarity == "5"
@@ -53,12 +57,12 @@ def select_artifact_rarity():
     while valid_rarity == False:
         rarity = user_input("What Rarity are you looking for?\n")
         if validate_rarity(rarity):
-            return rarity
+            return int(rarity)
         else:
             print("invalid rarity\n")
 
 
-def roll_artifact_main_stat(artifact_slot):
+def roll_artifact_main_stat(artifact_slot): #framework for math logic later
     if artifact_slot == "circlet":
         pass
     elif artifact_slot == "goblet":
@@ -84,11 +88,13 @@ def select_artifact_main_stat(artifact_slot):
         print("What Main Stat for the artifact are you looking for?\n")
         while valid_stat == False:
             main_stat = user_input("options are " + ", ".join(valid_main_stats_by_slot[artifact_slot]))
-            if main_stat in valid_main_stats_by_slot[artifact_slot]:
-                valid_stat = True
+            if validate_main_stat(artifact_slot, main_stat):
+                return main_stat
             else:
                 print("Invalid choice.\n")
-    return main_stat
+
+def validate_main_stat(slot, main_stat):
+    return main_stat in valid_main_stats_by_slot[slot]
 
 #def rollInitialOffStat(string mainStat)
 
@@ -96,7 +102,6 @@ def select_off_stats(rarity, main_stat):
     selected_rolls = 0
     if rarity == 4:
         selected_rolls += 1 #causes off_stat selection to loop one less time
-    sub_stats = ["hp", "hp%", "atk", "atk%", "def%", "def", "elemental mastery", "recharge%", "crit rate", "crit damage"]
     off_stats = []
     starting_rolls = user_input("Do you want the artifact to drop with full sub-stats?")
     if starting_rolls == "no":
@@ -105,13 +110,16 @@ def select_off_stats(rarity, main_stat):
         valid_choice = False
         while valid_choice == False:
             stat_choice = user_input("what substat do you want?\n")
-            if stat_choice in sub_stats and stat_choice != main_stat and stat_choice not in off_stats:
+            if validate_off_stats(stat_choice, off_stats, main_stat):
                 valid_choice = True
+                selected_rolls += 1
+                off_stats.append(stat_choice)
             else: 
                 print("Invalid Choice\n") 
-        selected_rolls += 1
-        off_stats.append(stat_choice)
     return off_stats
+
+def validate_off_stats(stat_choice, off_stats, main_stat):
+    return stat_choice in valid_sub_stats and stat_choice != main_stat and stat_choice not in off_stats
 
 #def select_off_stat_upgrade(off_stats):
     
@@ -126,11 +134,13 @@ def select_artifact_set(rarity):
     valid_choice = False
     while valid_choice == False:
         artifact_set = user_input("What set are you looking for?")
-        if artifact_set in valid_sets:
-            valid_choice = True
+        if validate_set_selection(artifact_set, valid_sets):
+            return artifact_set
         else:
             print("Invalid Choice\n")
-    return artifact_set
+
+def validate_set_selection(artifact_set, valid_sets):
+    return artifact_set in valid_sets
 
 def desired_artifact_input():
     rarity = select_artifact_rarity()
