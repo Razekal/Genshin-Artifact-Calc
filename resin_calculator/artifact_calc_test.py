09e9b87ff961ecf98f23de31fb7f8836cd1f90db
William Schuy <william.schuy@pcc.edu>
2021-01-07 08:03:09 -0800

    Update test cases

--- resin_calculator/artifact_calc_test.py
+++ resin_calculator/artifact_calc_test.py
@@ -13,3 +13,23 @@ def test_validate_rarity():
     assert artifact_calc.validate_rarity("2") == False
     assert artifact_calc.validate_rarity("1") == False
     assert artifact_calc.validate_rarity("6") == False
+
+def test_validate_set_selection_five():
+    valid_sets = ["gladiator's", "wanderer's troupe", "viridescent venerer", "thundering fury", "thundersoother"
+                  "crimson witch of flames", "lavawalker", "archaic petra", "retracing bolide", "maiden beloved"
+                  "noblesse oblige", "bloodstained chivalry"]
+    assert artifact_calc.validate_set_selection("gladiator's", valid_sets) == True
+    assert artifact_calc.validate_set_selection("instructor", valid_sets) == False
+    
+def test_validate_set_selection_four():
+    valid_sets = ["gladiator's", "wanderer's troupe", "viridescent venerer", "thundering fury", "thundersoother"
+                  "crimson witch of flames", "lavawalker", "archaic petra", "retracing bolide", "maiden beloved"
+                  "noblesse oblige", "bloodstained chivalry"]
+    #Copy paste from code for if rarity = 5:
+    valid_sets = valid_sets + ["instructor", "berserker", "exile", "resolution of sojourner", "martial artist", "defender's will", 
+                              "tiny miracle", "brave heart", "gambler", "scholar", "prayer"]
+    assert artifact_calc.validate_set_selection("gladiator's", valid_sets) == True
+    assert artifact_calc.validate_set_selection("instructor", valid_sets) == True
+    
+
+    
